914590f936aee26c1acc28180c72590f002bfe6f
mattding <mdin874@aucklanduni.ac.nz>
2021-06-09 14:01:29 +1200

    Added JSONUtils class and jackson library

--- src/com/swingapp/User.java
+++ src/com/swingapp/User.java
@@ -14,16 +14,37 @@ public class User {
     private int admin;
     private int numArticles;
 
-    public int getId() {return id;}
-    public String getFirstName() {return fname;}
-    public String getLastName() {return lname;}
-    public String getUsername() {return username;}
-    public String getDob() {return dob;}
-    public String getPassword() {return password;}
-    public String getDescription() {return description;}
-    public String getImageSource() {return imageSource;}
-    public String getAuthToken() {return authToken;}
-    public int getAdmin() {return admin;}
-    public int getNumArticles() {return numArticles;}
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirstName() { return fname; }
+    public void setFirstName(String fname) { this.fname = fname; }
+
+    public String getLastName() { return lname; }
+    public void setLastName(String lname) { this.lname = lname; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getDob() { return dob; }
+    public void setDob(String dob) { this.dob = dob; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getImageSource() { return imageSource; }
+    public void setImageSource(String imageSource) { this.imageSource = imageSource; }
+
+    public String getAuthToken() { return authToken; }
+    public void setAuthToken(String authToken) { this.authToken = authToken; }
+
+    public int getAdmin() { return admin; }
+    public void setAdmin(int admin) { this.admin = admin; }
+
+    public int getNumArticles() { return numArticles; }
+    public void setNumArticles(int numArticles) { this.numArticles = numArticles; }
 
 }
